7df7fcfec2d05369df4b1dcf48c7ba83b460f6c4
jvill171 <juanvillarreal64@yahoo.com>
2023-09-28 17:17:54 -0500

    Updated commented references

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-/** Express app for jobly. */
+/** Express app for magidekt. */
 
 const express = require("express");
 const cors = require("cors");
--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 "use strict";
-/** Database setup for jobly. */
+/** Database setup for magidekt. */
 const { Client } = require("pg");
 const { getDatabaseUri } = require("./config");
 
