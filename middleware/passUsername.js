3feb53806861c2fcf2afe508fdf90ec8e2b73987
jvill171 <juanvillarreal64@yahoo.com>
2023-11-08 18:13:11 -0600

    Added docstrings

--- middleware/passUsername.js
+++ middleware/passUsername.js
@@ -1,4 +1,15 @@
 
+/**passUsername
+ * Middleware to extract and store the username from request parameters.
+ *
+ * This middleware extracts the `username` parameter from the request and
+ * stores it as `req.username` for further processing by subsequent
+ * route handlers.
+ * 
+ * This middleware is meant to be used after another middleware
+ * which has verified a username exists
+ * 
+ */
 const passUsername = (req, res, next) => {
   const username = req.params.username;
   req.username = username;
