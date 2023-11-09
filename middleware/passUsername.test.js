8205afeff29cea040e033865e7ad94a5f1814742
jvill171 <juanvillarreal64@yahoo.com>
2023-11-08 18:13:30 -0600

    Created test for passUsername middleware

---
+++ middleware/passUsername.test.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const { UnauthorizedError } = require("../expressError");
+const { passUsername } = require("./passUsername");
+
+describe("passUsername", function (){
+    test("works", function (){
+        expect.assertions(1);
+        const req = { params: { username: "test" } };
+        const res = {};
+        const next = function (err) {
+          expect(err).toBeFalsy();
+        };
+        passUsername(req, res, next);
+    })
+})
+
